156e06d8449bf15ab2a862e5f80bc3b3787acb7d
Diego Vasquez <dialvf@hotmail.com>
2020-08-03 13:17:51 -0500

    TAREA:Proyecto 3-3

--- src/java/Modelo/Paciente.java
+++ src/java/Modelo/Paciente.java
@@ -10,5 +10,64 @@ package Modelo;
  * @author w10
  */
 public class Paciente {
+     private int idpaciente;
+    private String nombres;
+    private String apellido;
+    private String Enfermedad;
+    private int Tiempo;
+
+    public Paciente() {
+    }
+
+    public Paciente(int idpaciente, String nombres, String apellido, String Enfermedad, int Tiempo) {
+        this.idpaciente = idpaciente;
+        this.nombres = nombres;
+        this.apellido = apellido;
+        this.Enfermedad = Enfermedad;
+        this.Tiempo = Tiempo;
+    }
+
+    public int getIdpaciente() {
+        return idpaciente;
+    }
+
+    public void setIdpaciente(int idpaciente) {
+        this.idpaciente = idpaciente;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEnfermedad() {
+        return Enfermedad;
+    }
+
+    public void setEnfermedad(String Enfermedad) {
+        this.Enfermedad = Enfermedad;
+    }
+
+    public int getTiempo() {
+        return Tiempo;
+    }
+
+    public void setTiempo(int Tiempo) {
+        this.Tiempo = Tiempo;
+    }
+    
+    
+    
     
 }
